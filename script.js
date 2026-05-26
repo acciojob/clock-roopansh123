2a25b6a1b480b720f8305b49e334fec4c4727a61
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2026-05-26 17:09:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+
+function updateTimer() {
+    timer.textContent = new Date().toString();
+}
+
+updateTimer(); // display immediately
+setInterval(updateTimer, 1000); // update every second
